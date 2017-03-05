2ed25651f5fae4943bbfb12aa90015ab1f6f2014
Felix Heyn-Johnsen <fj@hest.local>
2017-03-05 15:52:21 +0100

    distinct code, exta packs

--- src/main/scala/Dimensions.scala
+++ src/main/scala/Dimensions.scala
@@ -5,11 +5,17 @@
 import scala.util.parsing.json._
 
 object Dimensions extends App {
+
   sealed trait PackType
+
   case object Fun extends PackType
+
   case object Team extends PackType
+
   case object Level extends PackType
+
   case object Story extends PackType
+
   case object Polybag extends PackType
 
   val adventureWorlds = List("DCComics",
@@ -44,13 +50,13 @@ object Dimensions extends App {
     "TheLEGOBatmanMovie"
   )
 
-  val levelWorlds = List("Vorton","BackToTheFuture","TheSimpsons","Portal2","DoctorWho", "GhostBusters", "MidwayArcade",
+  val levelWorlds = List("Vorton", "BackToTheFuture", "TheSimpsons", "Portal2", "DoctorWho", "GhostBusters", "MidwayArcade",
     "SonicTheHedgehog",
     "AdventureTime",
     "MissionImpossible",
     "TheGoonies"
   )
-  val storyWorlds = List("Ghostbusters2016","FantasticBeastsAndWhereToFindThem","TheLEGOBatmanMovie")
+  val storyWorlds = List("Ghostbusters2016", "FantasticBeastsAndWhereToFindThem", "TheLEGOBatmanMovie")
 
   type Ability = String
   type World = String
@@ -61,7 +67,6 @@ object Dimensions extends App {
   def distinctAbilities(packs: List[pack]): Set[Ability] = {
     packs.flatMap(p => p.abilities).toSet
   }
-
 
   //TODO worlds are adv, level, story etc. one function for each or perameter??
   def distinctWorlds(packs: Selection): Set[World] = {
@@ -76,7 +81,7 @@ object Dimensions extends App {
     distinctWorlds(packs.filter(p => p.packType == "story"))
   }
 
-  def json2pack(json: Map[String,Any]): pack = {
+  def json2pack(json: Map[String, Any]): pack = {
     pack(json.get("id").get.asInstanceOf[Double].toInt,
       json.get("name").get.asInstanceOf[String],
       json.get("packType").get.asInstanceOf[String],
@@ -87,21 +92,18 @@ object Dimensions extends App {
   }
 
   val allPacksJson = JSON.parseFull(scala.io.Source.fromFile("./src/main/resources/packs.json").getLines.mkString)
-  val allPacksAny = allPacksJson.get.asInstanceOf[Map[Any,Any]]("packs").asInstanceOf[List[Map[String,Any]]]
-
-
-  //TODO convert to
-  //val allPacks = allPacksJson.get.asInstanceOf[List[Any]]//("packs")
-  println(allPacksAny.head)
-  println(json2pack(allPacksAny.head))
+  val allPacksAny = allPacksJson.get.asInstanceOf[Map[Any, Any]]("packs").asInstanceOf[List[Map[String, Any]]]
 
   val allPacks = allPacksAny.map(ap => json2pack(ap))
-  for (ap <- allPacks) {
-    println(ap)
-  }
-  println(distinctAbilities(allPacks))
-}
+  val allAbilities = distinctAbilities(allPacks)
+  val allWorlds = distinctWorlds(allPacks)
 
+  val currentIds = List(71248, 71245, 71344, 71205, 71256, 71240, 71171, 71340, 71235, 71203, 71247, 71344, 71223, 71202)
+
+  val currentPacks: Selection = allPacks.filter(p => currentIds.contains(p.id))
+  val missingAbilities = allAbilities diff distinctAbilities(currentPacks)
+  println(s"missing abilities: $missingAbilities")
+}
 class Dimensions {
 
 }
